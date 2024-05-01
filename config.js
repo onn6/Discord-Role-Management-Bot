d46f3c098c7bfe1502a9f863a6ddc54257e0c04a
onn6 <155413897+onn6@users.noreply.github.com>
2024-05-01 08:29:07 +0300

    config.js

--- config.js
+++ config.js
@@ -11,64 +11,36 @@ Copyright (c) 2024 Wick Studio
 module.exports = {
     clientId: "1212347317793857546", // ايدي البوت
     guildId: "559015222405234697", // ايدي السيرفر
-    thumbnail: "", // الصورة المصغرة
-    image: "", // صورة الامبد الكبيرة
-    footer: "", // صورة مصغرة للفوتر
+    thumbnail: "https://media.discordapp.net/attachments/955662652959502426/1215187362326515733/image.png?ex=66328cac&is=66313b2c&hm=0388585811c8af097bcdd9aa851f46c5ec3994e3d16c64022744bb8f5f4a7c33&=&format=webp&quality=lossless&width=550&height=308", // الصورة المصغرة
+    image: "https://media.discordapp.net/attachments/955662652959502426/1215187362326515733/image.png?ex=66328cac&is=66313b2c&hm=0388585811c8af097bcdd9aa851f46c5ec3994e3d16c64022744bb8f5f4a7c33&=&format=webp&quality=lossless&width=550&height=308", // صورة الامبد الكبيرة
+    footer: "https://media.discordapp.net/attachments/955662652959502426/1215187362326515733/image.png?ex=66328cac&is=66313b2c&hm=0388585811c8af097bcdd9aa851f46c5ec3994e3d16c64022744bb8f5f4a7c33&=&format=webp&quality=lossless&width=550&height=308", // صورة مصغرة للفوتر
     roles: {
       section1: {
         label: "section_1", // اسم القسم الاول
         description: "Select your roles for Mentions", // وصف القسم
         roles: [
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
+          { label: "role_name", value: "955662596730662923" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
         ]
       },
       section2: {
         label: "section_2", // اسم القسم الثاني
         description: "Select your roles for Languages", // وصف القسم
         roles: [
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
+          { label: "role_name", value: "955662596730662923" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
         ]
       },
       section3: {
         label: "section_3", // اسم القسم الثالث
         description: "Select your roles for Colors", // وصف القسم
         roles: [
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
+          { label: "role_name", value: "955662596730662923" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
         ]
       },
       section4: {
         label: "section_3", // اسم القسم الثالث
-        description: "Select your roles for Colors", // وصف القسم
+        description: "Select your roles forD", // وصف القسم
         roles: [
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
-          { label: "role_name", value: "role_id" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
+          { label: "role_name", value: "955662596730662923" }, // role_name = اسم الرتبة | role_id = ايدي الرتبة
         ]
       },
     }
